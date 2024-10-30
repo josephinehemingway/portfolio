39826fd74b24be3d141ee4ee370119dee4785eb6
josephinehemingway <josephine.hemingway@gmail.com>
2024-10-31 01:16:59 +0800

    rephrased intro

--- josephinehemingway/src/components/about/about.tsx
+++ josephinehemingway/src/components/about/about.tsx
@@ -14,12 +14,12 @@ const About: React.FC<SectionProps> = ({sectionRef}) => {
                 I love creating nice things that make an impact.
             </h3>
             <p>
-                I'm an individual of many passions, passionate about combining design with technology to solve problems at the intersection of business. 
+                I'm passionate about many things, especially when design intersects with technology to solve problems. 
                 <b className='bold'>{' '} Seeing my ideas come to life is what truly captivates me.</b>
             </p>
             <p>
                 {/* I started a small home based business when I was 19, and I currently work as a software engineer after college.  */}
-                Outside of work, I channel my spare time learning, working on projects, and pursuing my artistic interests through creative projects in floral design, painting, photography and digital art. 
+                Outside of work, I channel my spare time learning, working on projects, playing sports and pursuing my artistic interests through creative projects in floral design, photography and exploring various art mediums. 
             </p>
         </div>
   )
